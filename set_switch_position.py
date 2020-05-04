e382b569c945aa3764067a039d589a69a9b3f025
satt99 <satt99kbd@gmail.com>
2020-05-04 18:13:27 +0900

    Fix switch position

--- set_switch_position.py
+++ set_switch_position.py
@@ -1,6 +1,9 @@
+from math import sin, cos, radians
+
+
 sw_pos = dict()
 with open('switch_position.csv') as f:
-    for line in f:
+    for i, line in enumerate(f):
         spline = line.split(',')
         label = spline[0].replace('comma', ',') # workaround since were reading a csv...
         x = float(spline[1])
@@ -12,13 +15,26 @@ with open('switch_position.csv') as f:
             r = 120
         else:
             r = 0
-        sw_pos[label] = [x, y, r]
+        sw_pos[label] = [x, y, r, i+1]
 
 # set switch position
 import pcbnew
 board = pcbnew.GetBoard()
 for key in sw_pos:
+    # set switch position
     sw = board.FindModuleByReference(key)
-    print key, sw_pos[key]
-    sw.SetPosition(pcbnew.wxPointMM(sw_pos[key][0],sw_pos[key][1]))
-    sw.SetOrientation(sw_pos[key][2])
+    params = sw_pos[key]
+    print key, params
+    sw.SetPosition(pcbnew.wxPointMM(params[0],params[1]))
+    sw.SetOrientation(params[2])
+    # set diode position
+    di = board.FindModuleByReference('D{}'.format(params[3]))
+    s = sin(radians(params[2]/10))
+    c = cos(radians(params[2]/10))
+    dx = 8.5 * c + 1.5 * s
+    dy = 8.5 * s - 1.5 * c
+    x = params[0] + dx
+    y = params[1] - dy
+    print x, y
+    di.SetPosition(pcbnew.wxPointMM(x, y))
+    di.SetOrientation(params[2]+900)
